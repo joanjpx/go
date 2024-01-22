eed5387a8ce7d6d702394ca4cc7e76809fdfdf52
Joan Perez <joansyn.s>
2024-01-21 21:56:49 -0500

    Fix

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # go
 Golang course for education purposes
+
+#### GO FRAMEWORK SENIORITY
+
+** Basics operators
+** HTTP Server
+** API REST
+** CRUD API
+** Send mail
+** Read CSV
+** CLI to Create
+        - Routes
+        - Controllers
+        - Middleware
--- src/hello/hello.go
+++ src/hello/hello.go
@@ -6,12 +6,35 @@ import (
 	"example.com/greetings"
 )
 
+
+var z int = 21
+
 func main() {
     // Get a greeting message and print it.
     // message := greetings.Hello("Gladys")
     
+    var message string = "Message";
+
+    // call function from another package/module
     fmt.Println(greetings.Hi("Vivian"))
     fmt.Println(greetings.Hello("Viv"))
     fmt.Println(greetings.Regards("Vivi"))
     fmt.Println(greetings.Bye("Vivian"))
+    fmt.Println(message)
+
+
+    
+
+    for i := 0; i < 10; i++ {
+        
+        fmt.Println(i);
+    }
+
+    Numero()
+}
+
+
+func Numero() {
+
+    fmt.Println(z)
 }
